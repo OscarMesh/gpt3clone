c68db4333d729c9c6870753edc39581445e0661c
Oscar Mesh <okerekeifeanyi65@gmail.com>
2022-01-24 14:53:08 +0100

    Updates

---
+++ precache-manifest.de3cbefc1417b745258100124d5921fc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deed9be61059a695bcb1acee0c93a568",
+    "url": "/gpt3clone/index.html"
+  },
+  {
+    "revision": "616701cf1a305e56da3f",
+    "url": "/gpt3clone/static/css/main.df65995f.chunk.css"
+  },
+  {
+    "revision": "d0a2e1765e0c2bb625e4",
+    "url": "/gpt3clone/static/js/2.efa40308.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/gpt3clone/static/js/2.efa40308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616701cf1a305e56da3f",
+    "url": "/gpt3clone/static/js/main.eb35f9a0.chunk.js"
+  },
+  {
+    "revision": "2be78ded78576fc544bc",
+    "url": "/gpt3clone/static/js/runtime-main.2b1a9794.js"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/gpt3clone/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/gpt3clone/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/gpt3clone/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/gpt3clone/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/gpt3clone/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/gpt3clone/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/gpt3clone/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/gpt3clone/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/gpt3clone/static/media/possibility.3c84ae26.png"
+  }
+]);
